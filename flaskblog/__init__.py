21528c6390e80073f1e3b58e6e50afa6155710b7
ukhanskyi <ukhanskyi@gmail.com>
2019-11-11 17:29:04 +0200

    add migrations

---
+++ flaskblog/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'Thisisasecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routes, models
